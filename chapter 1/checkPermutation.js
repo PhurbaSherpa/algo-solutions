cf691b29d360aa6b2ba0ab2f093f1da8cb20b94c
Phurba Sherpa <srpphurba@gmail.com>
2020-01-26 01:09:16 -0500

    implemented another permutation solution

--- chapter 1/checkPermutation.js
+++ chapter 1/checkPermutation.js
@@ -10,3 +10,20 @@ function checkPermutation(str1, str2) {
     .join("");
   return str1 === str2;
 }
+
+function checkPermutation(str1, str2) {
+  if (str1.length !== str2.length) return false;
+  let counter = {};
+  let arr = str1.split("");
+  for (let letter of arr) {
+    counter[letter] = counter[letter] ? counter[letter] + 1 : 1;
+  }
+  for (let i = 0; i < str2.length; i++) {
+    let char = str2[i];
+    letters[char]--;
+    if (letters[char] < 0) {
+      return false;
+    }
+  }
+  return true;
+}
