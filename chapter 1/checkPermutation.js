923bec7a777fccdfd73ce8dfd2ae459ee500368c
Phurba Sherpa <srpphurba@gmail.com>
2020-01-26 01:02:20 -0500

    changed permutation answer

--- chapter 1/checkPermutation.js
+++ chapter 1/checkPermutation.js
@@ -1,15 +1,5 @@
 function checkPermutation(str1, str2) {
   if (str1.length !== str2.length) return false;
-
-  for (let i = 0; i < str2.length; i++) {
-    if (!str2.inlcudes(str[i])) return false;
-  }
-  return true;
-}
-
-function checkPermutation(str1, str2) {
-  if (str1.length !== str2.length) return false;
-
   str1
     .split("")
     .sort()
@@ -18,14 +8,5 @@ function checkPermutation(str1, str2) {
     .split("")
     .sort()
     .join("");
-
-  let l1 = 0;
-  let l2 = 0;
-  let r1 = str1.length - 1;
-  let r2 = str.length - 1;
-
-  while (l1 < r1) {
-    if (str1[l1] !== str2[l2] || str1[r1] !== str1[r2]) return false;
-  }
-  return true;
+  return str1 === str2;
 }
