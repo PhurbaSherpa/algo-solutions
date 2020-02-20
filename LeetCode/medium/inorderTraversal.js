7cd25e59bbe815db1105571ce06646a8b2730094
Phurba Sherpa <srpphurba@gmail.com>
2020-02-20 18:31:19 -0500

    add iterative

--- LeetCode/medium/inorderTraversal.js
+++ LeetCode/medium/inorderTraversal.js
@@ -14,3 +14,22 @@ var inorderTraversal = function(root) {
 
   return values;
 };
+var inorderTraversal = function(root) {
+  const stack = [];
+  const values = [];
+
+  let current = root;
+
+  while (current || stack.length) {
+    if (current) {
+      stack.push(current);
+      current = current.left;
+    } else {
+      current = stack.pop();
+      values.push(current.val);
+      current = current.right;
+    }
+  }
+
+  return values;
+};
