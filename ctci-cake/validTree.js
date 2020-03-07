2951e0d52d82789ca17c87a1ffcd2479ff5e2d4b
Phurba Sherpa <srpphurba@gmail.com>
2020-03-07 17:02:56 -0500

    added valid tree

---
+++ ctci-cake/validTree.js
@@ -0,0 +1,32 @@
+function isBinarySearchTreeIterative(treeRoot) {
+  let q = [{ node: treeRoot, lower: -Infinity, upper: Infinity }];
+  while (q.length) {
+    const { node, lower, upper } = q.pop();
+
+    if (node.value <= lower || node.value >= upper) return false;
+
+    if (node.left) {
+      q.push({ node: node.left, lower, upper: node.value });
+    }
+    if (node.right) {
+      q.push({ node: node.right, lower: node.value, upper });
+    }
+  }
+  return true;
+}
+
+function isBinarySearchTreeRecursively(treeRoot, lowerBound, upperBound) {
+  lowerBound = lowerBound ? lowerBound : Number.NEGATIVE_INFINITY;
+  upperBound = upperBound ? upperBound : Number.POSITIVE_INFINITY;
+
+  if (!treeRoot) return true;
+
+  if (treeRoot.value >= upperBound || treeRoot.value <= lowerBound) {
+    return false;
+  }
+
+  return (
+    isBinarySearchTree(treeRoot.left, lowerBound, treeRoot.value) &&
+    isBinarySearchTree(treeRoot.right, treeRoot.value, upperBound)
+  );
+}
