2cc2e59bd5bc8b65e68648d2dd204689b0c7d5f5
Phurba Sherpa <srpphurba@gmail.com>
2020-03-08 20:40:40 -0400

    refactor race.js

--- random/race.js
+++ random/race.js
@@ -1,53 +1,47 @@
-// needs file ./findTimeDiff for the function findDifference
-
 // the race functions takes in an array of objects with each having a name and time property,
 // if racer didnt finish they have a time of "dnf",
 
 // if no one finshed the race return no winner
 // if only one winner return  === > (name of winner) won with no contest
 // if multiple finishers return the winner and how much time difference they have with second place
-// uses findDifference to calculate time difference between two runners time
 
-const finddifference = require("./findTimeDiff");
 function race(arr) {
-  let winner;
-  let smallesttime;
-  let difference = [];
-  let completed = 0;
-  for (let i = 0; i < arr.length; i++) {
-    let timeobj = arr[i].time;
-    if (timeobj === "dnf") continue;
-    completed++;
-    timeobj = timeobj.split(":").map(t => {
-      t = Number(t);
-      return t;
-    });
-    if (!smallesttime) {
-      smallesttime = timeobj;
-      difference = [timeobj[0], timeobj[1], timeobj[2]];
-    } else {
-      if (finddifference(smallesttime, timeobj)) {
-        difference = finddifference(smallesttime, timeobj);
-      } else {
-        continue;
-      }
-    }
-    smallesttime = timeobj;
-    winner = arr[i];
+  arr = arr.sort((a, b) => {
+    if (a.time < b.time) return -1;
+    if (a.time > b.time) return 1;
+    return 0;
+  });
+  console.log(arr);
+  if (arr[0].time === "dnf") {
+    return "No winners";
   }
-  console.log(
-    !winner
-      ? "There is no winner"
-      : completed === 1
-      ? `${winner.name} won by no contest`
-      : `${winner.name} won by ${difference[0]} hours, ${difference[1]} minutes, ${difference[2]} seconds`
-  );
-  return !winner
-    ? "There is no winner"
-    : completed === 1
-    ? `${winner.name} won by no contest`
-    : `${winner.name} won by ${difference[0]} hours, ${difference[1]} minutes, ${difference[2]} seconds`;
+  if (arr[1].time === "dnf") {
+    return `${arr[1].name} won with no contest`;
+  } else {
+    first = arr[0].time.split(":");
+    second = arr[1].time.split(":");
+    let diff = findDiff(first, second);
+    console.log(
+      `${arr[0].name} won by ${diff[0]} hours, ${diff[1]} mins, ${diff[2]} secs`
+    );
+    return `${arr[0].name} won by ${diff[0]} hours, ${diff[1]} mins, ${diff[2]} secs`;
+  }
 }
+function findDiff(first, second) {
+  let diff = [];
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] === second[i]) {
+      diff[i] = second[i] - first[i];
+    } else if (first[i] < second[i]) {
+      diff[i] = second[i] - first[i];
+    } else {
+      diff[i - 1]--;
+      diff[i] = 60 - (first[i] - second[i]);
+    }
+  }
+  return diff;
+}
+
 race([
   { name: "Samuel", time: "05:42:14" },
   { name: "Fred", time: "05:12:53" },
