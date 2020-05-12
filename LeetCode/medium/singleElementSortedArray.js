9411bed2fba230ea469989edf7892cb80d06c11f
Phurba Sherpa <srpphurba@gmail.com>
2020-05-12 18:17:45 -0400

    singleElement new solution

--- LeetCode/medium/singleElementSortedArray.js
+++ LeetCode/medium/singleElementSortedArray.js
@@ -1,8 +1,26 @@
 var singleNonDuplicate = function (nums) {
-  for (let i = 0; i < nums.length; i++) {
-    let left = nums[i - 1];
-    let mid = nums[i];
-    let right = nums[i + 1];
-    if (left !== mid && mid !== right) return mid;
+  if (nums.length === 1) return nums[0];
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let mid = Math.floor((right + left) / 2);
+
+    if (left === mid) {
+      return nums[left] === nums[left - 1] ? nums[right] : nums[left];
+    }
+    if (mid % 2 === 0) {
+      if (nums[mid] === nums[mid + 1]) {
+        left = mid;
+      } else {
+        right = mid;
+      }
+    } else {
+      if (nums[mid] === nums[mid - 1]) {
+        left = mid;
+      } else {
+        right = mid;
+      }
+    }
   }
 };
